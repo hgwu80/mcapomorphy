534683284524845c2454e0e035f57c76a86871f8
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-04-17 20:16:56 +0800

    remove null test in test-data.R

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -1,7 +1,2 @@
 testthat::context('Testing data')
 setwd(here::here(''))  # workspace is reset per file
-
-testthat::test_that('sample_100', {
-    testthat::expect_identical(length(sample_100), 100L);
-    testthat::expect_true(sum(sample_100) == 5050);
-});
