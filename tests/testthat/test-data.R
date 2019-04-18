18cb017b3880d074406d6e9855b8aaa671a55d58
Travis CI User <travis@example.org>
2019-04-18 10:17:42 +0000

    [skip ci] build pkgdown at 2019-04-18 10:17:42

--- tests/testthat/test-data.R
+++
@@ -1,2 +0,0 @@
-testthat::context('Testing data')
-setwd(here::here(''))  # workspace is reset per file
--- tests/testthat/test-mine.R
+++
@@ -1,5 +0,0 @@
-testthat::context("Testing mine")
-setwd(here::here(''))  # workspace is reset per file
-
-
-
